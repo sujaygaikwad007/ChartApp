50e825e2834e515f2e3122270d7ea30a88a83a95
Aniket Patil <aniket.p@reapmind.com>
2024-06-10 13:13:50 +0530

    Initial Commit

---
+++ ChartApp/ChartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartAppApp.swift
+//  ChartApp
+//
+//  Created by Aniket Patil on 10/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ChartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
